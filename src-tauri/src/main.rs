59f5e231eaa11fae6f0a9ab0ec56102d9096e79f
FX <felix.arbeithuber@gmail.com>
2022-04-15 23:26:23 +0200

    working state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,26 +3,28 @@
     windows_subsystem = "windows"
 )]
 
+mod handlers;
+mod helper;
+// mod window;
+
 extern crate libobs_recorder;
-use chrono::Local;
-use libobs_recorder::{framerate::Framerate, rate_control::Cqp, resolution::Resolution, *};
 
-use std::{
-    cmp::min,
-    env,
-    fs::create_dir,
-    fs::File,
-    io::{Read, Seek, SeekFrom},
-    path::PathBuf,
-    thread,
-    time::Duration,
+use handlers::*;
+use helper::{get_new_filepath, get_recordings};
+use libobs_recorder::{
+    framerate::Framerate, rate_control::Cqp, resolution::Resolution, Recorder, RecorderSettings,
 };
+use tauri::{CustomMenuItem, Runtime, SystemTray, SystemTrayMenu};
 
-use tauri::{
-    api::path::video_dir,
-    http::{HttpRange, ResponseBuilder},
-    CustomMenuItem, Manager, RunEvent, SystemTray, SystemTrayEvent, SystemTrayMenu, WindowEvent,
-};
+#[tauri::command]
+async fn get_recordings_list() -> Vec<String> {
+    let recordings = get_recordings();
+    let mut ret = Vec::<String>::new();
+    for path in recordings {
+        ret.push(path.into_os_string().into_string().unwrap());
+    }
+    ret
+}
 
 // use the mutex to let only one recording be active at a time.
 // the bool in the mutex is unused.
@@ -31,7 +33,10 @@ struct RecordState {
     recording: std::sync::Mutex<bool>,
 }
 #[tauri::command]
-async fn record(state: tauri::State<'_, RecordState>) -> Result<(), String> {
+async fn record<R: Runtime>(
+    state: tauri::State<'_, RecordState>,
+    window: tauri::Window<R>,
+) -> Result<(), String> {
     // get mutex and store it in _recording_lock which is only dropped when the function returns
     // _recording_lock gets dropped after recorder (dropped in reverse order of declaration)
     // => the lock is always released after the recorder has completely shutdown
@@ -52,7 +57,10 @@ async fn record(state: tauri::State<'_, RecordState>) -> Result<(), String> {
 
     let mut recorder = Recorder::get(settings);
     if recorder.start_recording() {
-        thread::sleep(Duration::from_secs(60));
+        let (sender, receiver) = std::sync::mpsc::channel::<bool>();
+        let id = window.listen("stop_record", move |_| sender.send(true).unwrap());
+        let _ = receiver.recv();
+        window.unlisten(id);
         recorder.stop_recording();
     }
 
@@ -65,121 +73,13 @@ fn main() {
         .add_item(CustomMenuItem::new("quit", "Quit"));
     let system_tray = SystemTray::new().with_menu(tray_menu);
     let app = tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![record])
+        .invoke_handler(tauri::generate_handler![get_recordings_list, record])
         .system_tray(system_tray)
-        .on_system_tray_event(|app, event| match event {
-            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-                "open" => {
-                    let window = app.get_window("main").unwrap();
-                    window.show().unwrap();
-                    window.set_focus().unwrap();
-                }
-                "quit" => {
-                    Recorder::shutdown();
-                    app.exit(0);
-                }
-                _ => {}
-            },
-            SystemTrayEvent::DoubleClick {
-                position: _,
-                size: _,
-                ..
-            } => {
-                let window = app.get_window("main").unwrap();
-                window.show().unwrap();
-                window.set_focus().unwrap();
-            }
-            _ => {}
-        })
-        .register_uri_scheme_protocol("stream", move |_app, request| {
-            let mut response = ResponseBuilder::new();
-            #[cfg(target_os = "windows")]
-            let path_str = request.uri().replace("stream://localhost/", "");
-            #[cfg(not(target_os = "windows"))]
-            let path = request.uri().replace("stream://", "");
-
-            if !path_str.ends_with(".mp4") {
-                return response.mimetype("text/plain").status(404).body(Vec::new());
-            }
-
-            let mut path = PathBuf::from("../");
-            path.push(path_str);
-
-            let content = File::open(path);
-            let mut content = match content {
-                Ok(c) => c,
-                Err(_) => return response.mimetype("text/plain").status(404).body(Vec::new()),
-            };
-
-            let mut buf = Vec::new();
-            let mut status_code = 200;
-
-            // if the webview sent a range header, we need to send a 206 in return
-            // Actually only macOS and Windows are supported. Linux will ALWAYS return empty headers.
-            if let Some(range) = request.headers().get("range") {
-                let file_size = content.metadata().unwrap().len();
-                let range = HttpRange::parse(range.to_str().unwrap(), file_size).unwrap();
-                let first_range = range.first();
-                if let Some(range) = first_range {
-                    let mut real_length = range.length;
-
-                    if range.length > file_size / 3 {
-                        real_length = min(file_size - range.start, 1024 * 400);
-                    }
-
-                    let last_byte = range.start + real_length - 1;
-                    status_code = 206;
-
-                    // Only macOS and Windows are supported, if you set headers in linux they are ignored
-                    response = response
-                        .header("Connection", "Keep-Alive")
-                        .header("Accept-Ranges", "bytes")
-                        .header("Content-Length", real_length)
-                        .header(
-                            "Content-Range",
-                            format!("bytes {}-{}/{}", range.start, last_byte, file_size),
-                        );
-
-                    content.seek(SeekFrom::Start(range.start))?;
-                    content.take(real_length).read_to_end(&mut buf)?;
-                } else {
-                    content.read_to_end(&mut buf)?;
-                }
-            }
-
-            response.mimetype("video/mp4").status(status_code).body(buf)
-        })
+        .on_system_tray_event(system_tray_event_handler)
+        .register_uri_scheme_protocol("video", video_protocol_handler)
         .manage(RecordState::default())
-        .setup(|_app| {
-            Recorder::init().unwrap();
-            Ok(())
-        })
+        .setup(setup_handler)
         .build(tauri::generate_context!())
         .expect("error while running tauri application");
-
-    app.run(|app_handle, e| match e {
-        RunEvent::WindowEvent {
-            label,
-            event: WindowEvent::CloseRequested { api, .. },
-            ..
-        } => {
-            api.prevent_close();
-            app_handle.get_window(&label).unwrap().hide().unwrap();
-        }
-        RunEvent::ExitRequested { api, .. } => {
-            api.prevent_exit();
-        }
-        _ => {}
-    });
-}
-
-fn get_new_filepath() -> String {
-    let filename = format!("{}", Local::now().format("%Y-%m-%d_%H-%M-%S.mp4"));
-    let mut vid_dir = video_dir().unwrap();
-    vid_dir.push(PathBuf::from("league_recordings"));
-    if !vid_dir.exists() {
-        let _ = create_dir(vid_dir.as_path());
-    }
-    vid_dir.push(PathBuf::from(filename));
-    vid_dir.to_str().unwrap().into()
+    app.run(run_handler);
 }
