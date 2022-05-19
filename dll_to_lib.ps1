f6b3c0d83501570fbe2131b8b2a74582eeb9b8be
FX <37913466+FFFFFFFXXXXXXX@users.noreply.github.com>
2022-05-20 00:52:53 +0200

    script to create .lib from .dll

---
+++ dll_to_lib.ps1
@@ -0,0 +1,4 @@
+# run in VisualStudio Developer Powershell
+echo "LIBRARY OBS" > obs.def; 
+echo "EXPORTS" >> obs.def;
+foreach($line in (dumpbin /exports obs.dll | select -skip 19 )) { echo (-split $line | Select-Object -Index 3) >> obs.def};
