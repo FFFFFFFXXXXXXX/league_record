d8531da7ee799ada8fd296f7a435263e5c1c6b0f
FX <felix.arbeithuber@gmail.com>
2022-05-23 02:31:51 +0200

    simplify and dont deserialize game data every sec

--- src-tauri/lol_rec/src/main.rs
+++ src-tauri/lol_rec/src/main.rs
@@ -5,11 +5,12 @@ use std::{
     io::stdin,
     path::PathBuf,
     process::exit,
-    sync::mpsc::{channel, TryRecvError},
+    sync::mpsc::{channel, RecvTimeoutError},
     thread,
     time::{Duration, Instant},
 };
 
+use bytes::Bytes;
 use libobs_recorder::{
     rate_control::{Cqp, Icq},
     Recorder, RecorderSettings, Window,
@@ -23,7 +24,7 @@ const WINDOW_TITLE: &str = "League of Legends (TM) Client";
 const WINDOW_CLASS: &str = "RiotWindowClass";
 const WINDOW_PROCESS: &str = "League of Legends.exe";
 
-const SLEEP_SECS: u64 = 3;
+const SLEEP_SECS: u64 = 1;
 
 const DEBUG: bool = false;
 
@@ -60,7 +61,7 @@ fn main() {
         exit(1);
     }
 
-    // create recordeer settings
+    // create recorder settings
     let filename = format!("{}", chrono::Local::now().format(cfg.filename_format()));
     if DEBUG {
         println!("filename: {}", &filename);
@@ -75,6 +76,7 @@ fn main() {
         exit(1);
     };
 
+    thread::sleep(Duration::from_secs(3));
     // start recording
     if !recorder.start_recording() {
         exit(1);
@@ -89,21 +91,20 @@ fn main() {
     let (sender, receiver) = channel::<_>();
     let thread = thread::spawn({
         let rec_folder = cfg.recordings_folder();
-        move || {
-            let client = create_client();
+        let client = create_client();
 
-            let mut init = true;
-            let mut data_delay = 0.0;
-            let mut game_data = Value::Null;
+        let mut init = true;
+        let mut data_delay = 0.0;
+        let mut game_data = Bytes::default();
+        move || {
             loop {
                 // update data if recording
                 let data = get_league_data(&client).unwrap_or_default();
-                if !data.is_null() {
+                if !data.is_empty() {
                     // store the delay between event time and recording time
                     // but only if recording delay is unset (<0.0)
                     if init {
-                        let ts = data["timestamp"].as_f64();
-                        if let Some(ts) = ts {
+                        if let Some(ts) = get_timestamp(&data) {
                             data_delay = instant.elapsed().as_secs_f64() - ts;
                             init = false;
                         }
@@ -111,22 +112,15 @@ fn main() {
                     game_data = data;
                 }
 
-                // if value received or disconnected => break
-                // checks for sender disconnect
-                match receiver.try_recv() {
-                    Err(TryRecvError::Empty) => {}
-                    _ => break,
-                }
                 // delay SLEEP_MS milliseconds waiting for stop event
-                // break if stop event received
-                // recv_timeout can't differentiate between timeout and disconnect
+                // break if stop event received or sender disconnected
                 match receiver.recv_timeout(Duration::from_secs(SLEEP_SECS)) {
-                    Ok(_) => break,
-                    _ => {}
+                    Ok(_) | Err(RecvTimeoutError::Disconnected) => break,
+                    Err(RecvTimeoutError::Timeout) => {}
                 }
             }
 
-            if !game_data.is_null() {
+            if !game_data.is_empty() {
                 save_metadata(rec_folder, filename, data_delay, game_data);
             }
         }
@@ -186,7 +180,7 @@ fn create_client() -> Client {
     return client;
 }
 
-fn get_league_data(client: &Client) -> Option<Value> {
+fn get_league_data(client: &Client) -> Option<Bytes> {
     let result = client
         .get("https://127.0.0.1:2999/liveclientdata/allgamedata")
         .header(ACCEPT, "application/json")
@@ -194,39 +188,48 @@ fn get_league_data(client: &Client) -> Option<Value> {
         .send()
         .ok()?;
 
-    let data = if result.status() == StatusCode::OK {
-        result.json::<Value>().ok()?
-    } else {
-        return None;
-    };
-
-    if data["events"]["Events"][0]["EventName"] != "GameStart" {
-        return None;
-    }
-
-    let mut player_info: Option<&Value> = None;
-    let player_array = data["allPlayers"].as_array()?;
-    for player in player_array {
-        if player["summonerName"] == data["activePlayer"]["summonerName"] {
-            player_info = Some(player);
-            break;
-        }
-    }
-    if let Some(info) = player_info {
-        Some(json!({
-            "playerName": data["activePlayer"]["summonerName"],
-            "championName": info["championName"],
-            "stats": info["scores"],
-            "events": data["events"]["Events"],
-            "gameMode": data["gameData"]["gameMode"],
-            "timestamp": data["gameData"]["gameTime"]
-        }))
+    if result.status() == StatusCode::OK {
+        result.bytes().ok()
     } else {
         None
     }
 }
 
-fn save_metadata(mut folder: PathBuf, filename: String, data_delay: f64, mut json: Value) {
+fn get_timestamp(bytes: &Bytes) -> Option<f64> {
+    let data: Value = match serde_json::from_slice(&bytes) {
+        Ok(data) => data,
+        Err(_) => return None,
+    };
+    data["gameData"]["gameTime"].as_f64()
+}
+
+fn deserialize_game_data(bytes: Bytes) -> Value {
+    let data: Value = match serde_json::from_slice(&bytes) {
+        Ok(data) => data,
+        Err(_) => Value::Null,
+    };
+
+    let mut player_info: &Value = &Value::Null;
+    let player_array = data["allPlayers"].as_array().unwrap();
+    for player in player_array {
+        if player["summonerName"] == data["activePlayer"]["summonerName"] {
+            player_info = player;
+            break;
+        }
+    }
+
+    json!({
+        "playerName": data["activePlayer"]["summonerName"],
+        "championName": player_info["championName"],
+        "stats": player_info["scores"],
+        "events": data["events"]["Events"],
+        "gameMode": data["gameData"]["gameMode"]
+    })
+}
+
+fn save_metadata(mut folder: PathBuf, filename: String, data_delay: f64, data: Bytes) {
+    let mut json = deserialize_game_data(data);
+
     let mut result = Value::Null;
 
     let player_name = json["playerName"].clone();
