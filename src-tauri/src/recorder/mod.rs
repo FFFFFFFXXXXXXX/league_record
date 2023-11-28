aa4ccd34f62cfd5d3b75986f47348043afe128f5
FX <37913466+FFFFFFFXXXXXXX@users.noreply.github.com>
2023-11-28 01:23:35 +0100

    workaround for riot id not matching summoner_name & improved recorder

--- src-tauri/src/recorder/mod.rs
+++ src-tauri/src/recorder/mod.rs
@@ -1,10 +1,7 @@
 use std::{
     cmp::Ordering,
     path::{Path, PathBuf},
-    sync::{
-        mpsc::{channel, RecvTimeoutError},
-        Arc, Mutex,
-    },
+    sync::mpsc::{channel, RecvTimeoutError},
     thread,
     time::{Duration, Instant},
 };
@@ -20,12 +17,14 @@ use shaco::{
         ingame::{ChampionKill, DragonType, GameEvent, GameResult, Killer},
         ws::LcuSubscriptionType::JsonApiEvent,
     },
+    rest::RESTClient,
     ws::LcuWebsocketClient,
 };
 use tauri::{
     async_runtime::{self, JoinHandle},
     AppHandle, Manager,
 };
+use tokio::time::timeout;
 use tokio_util::sync::CancellationToken;
 #[cfg(target_os = "windows")]
 use windows::{
@@ -79,14 +78,10 @@ pub fn start(app_handle: &AppHandle) {
 
         enum State {
             Idle,
-            Recording,
+            Recording((JoinHandle<()>, CancellationToken)),
         }
 
         let mut state = State::Idle;
-
-        // use Options to 'store' values between loops
-        let recorder: Arc<Mutex<Option<Recorder>>> = Arc::new(Mutex::new(None));
-        let mut game_data_thread: Option<(JoinHandle<()>, CancellationToken)> = None;
 
         'recorder: loop {
             match state {
@@ -135,7 +130,7 @@ pub fn start(app_handle: &AppHandle) {
                     settings.record_audio(settings_state.get_audio_source());
                     settings.set_output_path(filename_path.to_str().expect("error converting filename path to &str"));
 
-                    let mut rec = match Recorder::new_with_paths(
+                    let mut recorder = match Recorder::new_with_paths(
                         Some(Path::new("./libobs/extprocess_recorder.exe")),
                         None,
                         None,
@@ -151,102 +146,81 @@ pub fn start(app_handle: &AppHandle) {
                         }
                     };
 
-                    let configured = rec.configure(&settings);
+                    let configured = recorder.configure(&settings);
                     if debug_log {
                         println!("recorder configured: {configured:?}");
-                        println!("Available encoders: {:?}", rec.available_encoders());
-                        println!("Selected encoder: {:?}", rec.selected_encoder());
+                        println!("Available encoders: {:?}", recorder.available_encoders());
+                        println!("Selected encoder: {:?}", recorder.selected_encoder());
                     }
                     if configured.is_err() {
                         break 'inner;
                     }
 
-                    *recorder.lock().unwrap() = Some(rec);
-
                     // --- ingame data collection ---
                     let cancel_token = CancellationToken::new();
-                    let app_handle = app_handle.clone();
                     let handle = async_runtime::spawn({
+                        // preparation for task
+                        let app_handle = app_handle.clone();
                         let cancel_subtoken = cancel_token.child_token();
-                        let recorder = Arc::clone(&recorder);
-
                         let mut outfile = settings_state.get_recordings_path().join(filename_path);
                         outfile.set_extension("json");
 
-                        async move {
-                            collect_ingame_data(app_handle, cancel_subtoken, recorder, outfile, debug_log).await;
-                        }
+                        // actual task
+                        async move { collect_ingame_data(&app_handle, cancel_subtoken, recorder, outfile, debug_log).await }
                     });
                     if debug_log {
                         println!("ingame task spawned: {handle:?}");
                     }
 
-                    game_data_thread = Some((handle, cancel_token));
-                    state = State::Recording;
+                    state = State::Recording((handle, cancel_token));
                 }
-                State::Recording => 'inner: {
-                    // stop if LoL window closed
+                State::Recording((mut handle, cancel_token)) => 'inner: {
+                    // don't stop while LoL window is open
                     if get_lol_window().is_some() {
+                        state = State::Recording((handle, cancel_token));
+                        break 'inner;
+                    } else {
+                        state = State::Idle;
+                    }
+
+                    // if task already finished -> done
+                    if handle.inner().is_finished() {
                         break 'inner;
                     }
 
-                    // stop recorder
-                    if let Some(mut rec) = recorder.lock().unwrap().take() {
-                        let stopped = rec.stop_recording();
-                        let shutdown = rec.shutdown();
-
-                        if debug_log {
-                            println!("recorder stopped: {stopped:?}");
-                            println!("recorder shutdown: {shutdown:?}");
+                    // spawn async thread to cleanup the gamedata task
+                    async_runtime::spawn(async move {
+                        // wait for 90s for EOG lobby before trying to cancel the task
+                        if timeout(Duration::from_secs(90), &mut handle).await.is_ok() {
+                            return;
+                        } else {
+                            cancel_token.cancel();
                         }
 
-                        set_recording_tray_item(&app_handle, false);
-                    };
-
-                    // spawn async thread to cleanup the game_data_thread if it doesn't exit by itself
-                    if let Some((mut handle, cancel_token)) = game_data_thread.take() {
-                        if handle.inner().is_finished() {
-                            break 'inner;
+                        // abort task if the cancel_token didn't stop the it (after 5s)
+                        if timeout(Duration::from_secs(5), &mut handle).await.is_err() {
+                            handle.abort();
                         }
-
-                        async_runtime::spawn(async move {
-                            // wait for 90s for EOG lobby before trying to cancel the task
-                            match tokio::time::timeout(Duration::from_secs(90), &mut handle).await {
-                                Ok(_) => return,
-                                Err(_) => cancel_token.cancel(),
-                            }
-                            // abort task if the cancel_token didn't stop the it (after 5s)
-                            if tokio::time::timeout(Duration::from_secs(5), &mut handle).await.is_err() {
-                                handle.abort();
-                            }
-                        });
-                    }
-
-                    state = State::Idle;
+                    });
                 }
             }
 
             // break if stop event received or sender disconnected
             match rx.recv_timeout(Duration::from_secs(1)) {
                 Ok(_) | Err(RecvTimeoutError::Disconnected) => {
-                    // stop recorder if running
-                    if let Some(mut rec) = recorder.lock().unwrap().take() {
-                        let stopped = rec.stop_recording();
-                        let shutdown = rec.shutdown();
+                    // cleanup the gamedata task if it doesn't exit by itself
+                    if let State::Recording((handle, cancel_token)) = state {
+                        if !handle.inner().is_finished() {
+                            cancel_token.cancel();
 
-                        if debug_log {
-                            println!("recorder stopped: {stopped:?}|{shutdown:?}");
+                            // give the task a little bit of time to complete a fs::write or sth
+                            std::thread::sleep(Duration::from_millis(500));
+
+                            // if it is still not finished -> abort
+                            if !handle.inner().is_finished() {
+                                handle.abort();
+                            }
                         }
-
-                        set_recording_tray_item(&app_handle, false);
-                    };
-
-                    // cleanup the game_data_thread if it doesn't exit by itself
-                    if let Some((handle, cancel_token)) = game_data_thread.take() {
-                        cancel_token.cancel();
-                        // give the task a little bit of time to complete a fs::write or sth
-                        std::thread::sleep(Duration::from_millis(250));
-                        handle.abort();
                     }
 
                     break 'recorder;
@@ -264,9 +238,9 @@ pub fn start(app_handle: &AppHandle) {
 }
 
 async fn collect_ingame_data(
-    app_handle: AppHandle,
+    app_handle: &AppHandle,
     cancel_subtoken: CancellationToken,
-    recorder: Arc<Mutex<Option<Recorder>>>,
+    mut recorder: Recorder,
     outfile: PathBuf,
     debug_log: bool,
 ) {
@@ -282,7 +256,13 @@ async fn collect_ingame_data(
         // busy wait for API
         // "sleep" by selecting either the next timer tick or the token cancel
         tokio::select! {
-            _ = cancel_subtoken.cancelled() => return,
+            _ = cancel_subtoken.cancelled() => {
+                let shutdown = recorder.shutdown();
+                if debug_log {
+                    println!("recorder shutdown: {shutdown:?}");
+                }
+                return;
+            }
             _ = timer.tick() => {}
         }
     }
@@ -292,6 +272,10 @@ async fn collect_ingame_data(
         if debug_log {
             println!("spectator game detected - aborting");
         }
+        let shutdown = recorder.shutdown();
+        if debug_log {
+            println!("recorder shutdown: {shutdown:?}");
+        }
         return;
     } else if debug_log {
         println!("game started")
@@ -299,24 +283,35 @@ async fn collect_ingame_data(
 
     let mut game_data = data::GameData::default();
     if let Ok(data) = ingame_client.all_game_data(None).await {
-        if debug_log {
-            println!("initial data recieved: {data:?}");
+        game_data.game_info.game_mode = data.game_data.game_mode.to_string();
+
+        // weird block instead of nested 'if let' to avoid: future is not `Send` as this value is used across an await
+        'label: {
+            // this is a workaround for https://github.com/RiotGames/developer-relations/issues/857
+            // Riot bug: the ingame API active_player summoner_name != playerlist summoner_name (because of Riot ID introduction)
+            // for old/normal code see git history
+            let Ok(client) = RESTClient::new() else { break 'label };
+            let Ok(json) = client.get("/lol-summoner/v1/current-summoner").await else {
+                break 'label;
+            };
+            let Some(game_name) = json["gameName"].as_str() else { break 'label };
+            game_data.game_info.summoner_name = game_name.to_owned();
+
+            if debug_log {
+                println!("current summoner: {json:?}");
+            }
         }
 
-        game_data.game_info.game_mode = data.game_data.game_mode.to_string();
-        // unwrap because active player always exists in livegame which we check for above
-        game_data.game_info.summoner_name = data.active_player.unwrap().summoner_name;
-        game_data.game_info.champion_name = data
-            .all_players
-            .into_iter()
-            .find_map(|p| {
-                if p.summoner_name == game_data.game_info.summoner_name {
-                    Some(p.champion_name)
-                } else {
-                    None
-                }
-            })
-            .unwrap();
+        let champion_name = data.all_players.into_iter().find_map(|p| {
+            if p.summoner_name == game_data.game_info.summoner_name {
+                Some(p.champion_name)
+            } else {
+                None
+            }
+        });
+        if let Some(champion_name) = champion_name {
+            game_data.game_info.champion_name = champion_name;
+        }
     }
 
     if debug_log {
@@ -324,40 +319,31 @@ async fn collect_ingame_data(
     }
 
     // if initial game_data is successful => start recording
-    if let Some(rec) = recorder.lock().unwrap().as_mut() {
-        let start_recording = rec.start_recording();
+    let start_recording = recorder.start_recording();
+    if debug_log {
+        println!("start recording: {start_recording:?}");
+    }
 
+    if start_recording.is_err() {
+        // if recording start failed stop recording just in case and retry next 'recorder loop
+        let stop_recording = recorder.stop_recording();
+        let shutdown = recorder.shutdown();
         if debug_log {
-            println!("start recording: {start_recording:?}");
+            println!("recording start failed - stop recording: {stop_recording:?}");
+            println!("recorder shutdown: {shutdown:?}");
         }
-
-        if start_recording.is_err() {
-            // if recording start failed stop recording just in case and retry next loop
-            let stop_recording = rec.stop_recording();
-            if debug_log {
-                println!("start failed - stop recording: {stop_recording:?}");
-            }
-            return;
-        }
-    } else {
-        if debug_log {
-            println!("error getting recorder from Mutex - aborting");
-        }
+        set_recording_tray_item(&app_handle, false);
         return;
     }
 
-    // recording started
-    let recording_start = Some(Instant::now());
+    let recording_start = Instant::now();
     set_recording_tray_item(&app_handle, true);
 
-    // get values from Options that are always Some
-    let mut ingame_events = EventStream::from_ingame_client(ingame_client, None);
-    let recording_start = recording_start.as_ref().unwrap();
-
     if debug_log {
-        println!("Started EventStream - listening to ingame events");
+        println!("Starting EventStream - listening to ingame events");
     }
 
+    let mut ingame_events = EventStream::from_ingame_client(ingame_client, None);
     while let Some(event) =
         tokio::select! { event = ingame_events.next() => event, _ = cancel_subtoken.cancelled() => None }
     {
@@ -413,6 +399,14 @@ async fn collect_ingame_data(
     if debug_log {
         println!("Ingame window has closed");
     }
+
+    let stopped = recorder.stop_recording();
+    let shutdown = recorder.shutdown();
+    if debug_log {
+        println!("recorder shutdown: {shutdown:?}");
+        println!("recorder stopped: {stopped:?}");
+    }
+    set_recording_tray_item(&app_handle, false);
 
     // after the game client closes wait for LCU websocket End Of Game event
     let mut ws_client = match LcuWebsocketClient::connect().await {
