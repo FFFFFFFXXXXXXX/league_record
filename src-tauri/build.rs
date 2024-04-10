3681cbbb1859dbcac1bcec2964c57c0b24166c2b
FX <fffffffxxxxxxxfffffffxxxxxxx@gmail.com>
2024-04-10 02:37:38 +0200

    update libobs-recorder dependency

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,17 +1,7 @@
 fn main() {
-    build_helper::build_to_path("./").unwrap();
-    copy_libobs_recorder_dependencies();
+    const LIBOBS_DIR: &str = "./";
+    build_helper::build_to_path(LIBOBS_DIR).unwrap();
+    build_helper::copy_artifact_dependency_to_path(LIBOBS_DIR).unwrap();
+
     tauri_build::build();
-}
-
-fn copy_libobs_recorder_dependencies() -> Option<()> {
-    use std::{env, fs, path};
-
-    let artifact_path = env::var_os("CARGO_BIN_FILE_LIBOBS_RECORDER_extprocess_recorder")?;
-    let manifest_dir = env::var_os("CARGO_MANIFEST_DIR")?;
-    let target_path = path::Path::new(&manifest_dir).join("libobs/extprocess_recorder.exe");
-
-    fs::copy(artifact_path, target_path).ok()?;
-
-    Some(())
 }
